7a51d796e2ae67f8d91dec721c54ea55c492365d
Pushpanshu Ranjan Singh <pushpanshuranjansingh@gmail.com>
2020-06-29 13:53:23 +0530

    Initial Commit

---
+++ Udmey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udmey
+//
+//  Created by Pushpanshu Ranjan Singh on 29/06/20.
+//  Copyright © 2020 Pushpanshu Ranjan Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
